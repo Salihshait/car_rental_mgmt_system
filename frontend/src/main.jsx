e5ea49a6b691ac38447544d7c5cb0788222a06e6
Salihshait <salihshait3@gmail.com>
2026-08-04 01:26:44 +0530

    new4

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './store/store';
 import { appTheme } from './theme/appTheme';
 import App from './App';
@@ -10,12 +10,12 @@ import App from './App';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <ThemeProvider theme={appTheme}>
           <CssBaseline />
           <App />
         </ThemeProvider>
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
